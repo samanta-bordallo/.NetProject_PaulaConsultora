0c3c36a1964591f64ba9d7c11b9e4aff9afb37f8
samanta-bordallo <bordallo.samanta@gmail.com>
2024-02-08 15:55:14 +0000

    start project

---
+++ testeSiteIrmaMassa/Models/AboutMeModel.cs
@@ -0,0 +1,7 @@
+﻿namespace testeSiteIrmaMassa.Models
+{
+    public class AboutMeModel
+    {
+       public int Id { get; set; }
+    }
+}
---
+++ testeSiteIrmaMassa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testeSiteIrmaMassa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
